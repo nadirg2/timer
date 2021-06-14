e5d32ac4fe028983c85d638d5c77a8caee60e228
nadirg2 <gulievnadir3@gmail.com>
2021-06-14 12:08:46 +0300

    add start, pause, stop and resume buttons.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var countdown_id = null
+var start_ss = null
+var start_mm = null
+var start_hh = null
 
-var menu = document.getElementsByClassName("menu")[0];
+var content = document.querySelector(".content");
 var menu_items = document.querySelectorAll(".menu-item");
 menu_items.forEach((i) => {
   i.addEventListener("click", () => {
     const i_name = i.getAttribute("data-name");
-    menu.setAttribute("data-active", i_name);
+    content.setAttribute("data-active", i_name);
   });
 });
 
@@ -54,6 +57,7 @@ const startTimer = () => {
     let hh = timer_items[0].querySelector('.timer-item__input')
     if (hh.value=='00' && mm.value=='00' && ss.value=='00') {
         clearInterval(countdown_id)
+        countdown_id = null
     } else if (ss.value=='00' && mm.value=='00') {
         ss.value = '59'
         mm.value = '59'
@@ -66,7 +70,34 @@ const startTimer = () => {
     }
 }
 
-var start_timer_button = document.getElementById('button_start-timer')
+var start_timer_button = document.querySelector('#start-button')
 start_timer_button.addEventListener('click', () => {
+    let timer_items = document.querySelectorAll('.timer-item')
+    start_ss = timer_items[2].querySelector('.timer-item__input').value
+    start_mm = timer_items[1].querySelector('.timer-item__input').value
+    start_hh = timer_items[0].querySelector('.timer-item__input').value
+    document.querySelector('.content').setAttribute('data-state', 'start')
+    countdown_id = setInterval(startTimer, 1000)
+})
+
+var pause_timer_button = document.querySelector('#pause-button')
+pause_timer_button.addEventListener('click', () => {
+    clearInterval(countdown_id)
+    document.querySelector('.content').setAttribute('data-state', 'pause')
+})
+
+var stop_timer_button = document.querySelector('#stop-button')
+stop_timer_button.addEventListener('click', () => {
+    clearInterval(countdown_id)
+    let timer_items = document.querySelectorAll('.timer-item')
+    timer_items[2].querySelector('.timer-item__input').value = start_ss 
+    timer_items[1].querySelector('.timer-item__input').value = start_mm
+    timer_items[0].querySelector('.timer-item__input').value = start_hh
+    document.querySelector('.content').setAttribute('data-state', 'stop')
+})
+
+var resume_timer_button = document.querySelector('#resume-button')
+resume_timer_button.addEventListener('click', () => {
+    document.querySelector('.content').setAttribute('data-state', 'start')
     countdown_id = setInterval(startTimer, 1000)
 })
